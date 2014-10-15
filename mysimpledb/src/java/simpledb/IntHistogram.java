4e42d1bf5bc97c3bdd272fdc56328e8992b79b93
Yuxin David Huang <dhuang@colgate.edu>
2014-10-15 17:44:37 -0400

    changed to java7 compiler; finished lab 4 part 1, all tests pass

--- mysimpledb/src/java/simpledb/IntHistogram.java
+++ mysimpledb/src/java/simpledb/IntHistogram.java
@@ -4,6 +4,18 @@ package simpledb;
  * A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+	
+	private final int max;
+	
+	private final int min;
+	
+	private final int width;
+	
+	private final int lstWidth;
+	
+	private final int[] buckets;
+	
+	private int numTuples;
 
     /**
      * Create a new IntHistogram.
@@ -22,8 +34,30 @@ public class IntHistogram {
      * @param max     The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-        // some code goes here
-    }
+        // set min and max
+        this.min = min;
+        this.max = max;
+        
+        // calculate # of buckets
+        if (max-min+1 < buckets) {
+        	this.buckets = new int[max-min+1];
+        	width = 1;
+        	lstWidth = 1;
+        } else {
+        	this.buckets = new int[buckets];
+        	width = (int) Math.floor((max-min+1)/(double) buckets);
+        	lstWidth = (max-(min+(buckets-1)*width)) + 1;
+        }
+        
+        // set everything to 0, for safety
+        for (int i = 0; i < this.buckets.length; i++) {
+        	this.buckets[i] = 0;
+        }
+        
+        // set numTuples to 0
+        numTuples = 0;
+    } // end IntHistogram(int, int, int)
+    
 
     /**
      * Add a value to the set of values that you are keeping a histogram of.
@@ -31,8 +65,20 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-        // some code goes here
-    }
+    	// argument checking
+        if (v < min || v > max) {
+        	throw new IllegalArgumentException("value to add out of range");
+        }
+        
+        if ((v-min)/width < buckets.length-1) {
+        	buckets[(v-min)/width]++;		// not last bucket
+        } else {
+        	buckets[buckets.length-1]++;	// last bucket gets all remainder
+        }
+        
+        numTuples++;
+    } // end addValue(int)
+    
 
     /**
      * Estimate the selectivity of a particular predicate and operand on this table.
@@ -45,16 +91,134 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
+        switch (op) {
+        	case EQUALS:
+        		if (v < min || v > max) return 0;
+        		
+        		int b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		if (b < buckets.length-1) {
+        			return (buckets[b]/(double) width)/numTuples;
+        		} else {
+        			return (buckets[b]/(double) lstWidth)/numTuples;
+        		}
+        	case LIKE:
+        		if (v < min || v > max) return 0;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		if (b < buckets.length-1) {
+        			return (buckets[b]/(double) width)/numTuples;
+        		} else {
+        			return (buckets[b]/(double) lstWidth)/numTuples;
+        		}
+        	case NOT_EQUALS:
+        		if (v < min || v > max) return 1;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		if (b < buckets.length-1) {
+        			return 1 - (buckets[b]/(double) width)/numTuples;
+        		} else {
+        			return 1 - (buckets[b]/(double) lstWidth)/numTuples;
+        		}
+        	case GREATER_THAN:
+        		if (v < min) return 1;
+        		else if (v >= max) return 0;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        		
+        		double b_f = buckets[b]/(double) numTuples;
+        		if (b < buckets.length-1) {
+        			double rv = ((min+(b+1)*width-1-v)/(double) width) * b_f;
+        			for (int i = b+1; i < buckets.length; i++) {
+        				rv += buckets[i]/(double) numTuples;
+        			}
+        			return rv;
+        		} else {
+        			return ((max-v)/(double) lstWidth) * b_f;
+        		}
+        	case LESS_THAN_OR_EQ:
+        		if (v < min) return 0;
+        		else if (v >= max) return 1;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		b_f = buckets[b]/(double) numTuples;
+        		if (b < buckets.length-1) {
+        			b_f = buckets[b]/(double) numTuples;
+        			double rv = ((min+(b+1)*width-1-v)/(double) width) * b_f;
+        			for (int i = b+1; i < buckets.length; i++) {
+        				rv += buckets[i]/(double) numTuples;
+        			}
+        			return 1 - rv;
+        		} else {
+        			return 1 - ((max-v)/(double) lstWidth) * b_f;
+        		}
+        	case LESS_THAN:
+        		if (v <= min) return 0;
+        		else if (v > max) return 1;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		b_f = buckets[b]/(double) numTuples;
+        		if (b < buckets.length-1) {
+        			double rv = (((min+(b+1)*width)-v-1)/(double) width) * b_f;
+        			for (int i = b-1; i >= 0; i--) {
+        				rv += buckets[i]/(double) numTuples;
+        			}
+        			return rv;
+        		} else {
+        			double rv = ((max-v)/(double) lstWidth) * b_f;
+        			for (int i = b-1; i >= 0; i--) {
+        				rv += buckets[i]/(double) numTuples;
+        			}
+        			return rv;
+        		}
+        	case GREATER_THAN_OR_EQ:
+        		if (v <= min) return 1;
+        		else if (v > max) return 0;
+        		
+        		b = (v-min)/width < buckets.length-1 ?
+        				(v-min)/width : buckets.length-1;
+        				
+        		b_f = buckets[b]/(double) numTuples;
+        		if (b < buckets.length-1) {
+        			double rv = (((min+(b+1)*width)-v)/(double) width) * b_f;
+        			for (int i = b+1; i < buckets.length; i++) {
+        				rv += buckets[i]/(double) numTuples;
+        			}
+        			return rv;
+        		} else {
+        			return ((max-v+1)/(double) lstWidth) * b_f;
+        		}
+        	default:
+        		throw new IllegalArgumentException("op not supported");
+        }
+    } // end estimateSelectivity(Predicate.Op, int)
 
-        // some code goes here
-        return -1.0;
-    }
-
+    
     /**
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // some code goes here
-        return null;
-    }
-}
+        String rv = "All buckets are right exclusive except for the last one:\r\n";
+        for (int i = 0; i < buckets.length; i++) {
+        	if (i < buckets.length-1) {
+        		rv += "  " + (min+i*(max-min)) + "-" + (min+(i+1)*(max-min))
+        				+ ": " + buckets[i] + "\r\n";
+        	} else {
+        		rv += "  " + (min+i*(max-min)) + "-" + max
+        				+ ": " + buckets[i];
+        	}
+        }
+        
+        return rv;
+    } // end toString()
+} // end IntHistogram
